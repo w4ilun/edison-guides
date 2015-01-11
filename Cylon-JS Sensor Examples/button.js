ec982ea6ff0312c101a0daaa580ee5238ecfd25c
Rex St John <rstjohn@rstjohn-mac01.local>
2015-01-11 15:13:08 -0500

    button

---
+++ Cylon-JS Sensor Examples/button.js
@@ -0,0 +1,18 @@
+// Activate the touch button
+var Cylon = require('cylon');
+
+Cylon
+  .robot()
+  .connection('edison', { adaptor: 'intel-iot' })
+  .device('touch', { driver: 'button', pin: 3, connection: 'edison' })
+  .on('ready', function(my) {
+    my.touch.on('press', function() {
+      console.log('detected press');
+    });
+
+    my.touch.on('release', function() {
+	   console.log('touch released');
+    });
+  });
+
+Cylon.start();
