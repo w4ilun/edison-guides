e70703e86f4bb06c9f5bec23b643b6afbb3fc954
Wai Lun <w4ilun@yahoo.com>
2015-02-05 14:33:08 -0500

    added relay example

---
+++ recipies/Cylon-JS Sensor Examples/relay.js
@@ -0,0 +1,17 @@
+// Blink an LED
+// Pin 13 is the default pin on the large Arduino breakout board, change as needed.
+var Cylon = require('cylon');
+
+Cylon
+  .robot()
+  .connection('edison', { adaptor: 'intel-iot' })
+  .device('relay', { driver: 'direct-pin', pin: 4, connection: 'edison' })
+  .on('ready', function(my) {
+    var state = 0;
+    setInterval(function() {
+      state = 1 - state;
+      my.relay.digitalWrite(state);
+    }, 1000);
+  });
+
+Cylon.start();
