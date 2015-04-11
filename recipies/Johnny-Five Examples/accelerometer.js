b645aef20d3178fe1d2c3c440aa96b8616490706
Rex St. John <rexstjohn@gmail.com>
2015-04-11 19:31:17 -0400

    Update accelerometer.js

--- recipies/Johnny-Five Examples/accelerometer.js
+++ recipies/Johnny-Five Examples/accelerometer.js
@@ -1,1 +1,31 @@
+// "ADXL335"
+var five = require("johnny-five");
+var Edison = require("edison-io");
 
+var board = new five.Board({
+  io: new Edison()
+});
+
+board.on("ready", function() {
+  var accelerometer = new five.Accelerometer({
+    controller: "ADXL335",
+    pins: ["A0", "A1", "A2"]
+  });
+  
+  accelerometer.on("change", function() {
+    console.log("accelerometer");
+    console.log("  x            : ", this.x);
+    console.log("  y            : ", this.y);
+    console.log("  z            : ", this.z);
+    console.log("  pitch        : ", this.pitch);
+    console.log("  roll         : ", this.roll);
+    console.log("  acceleration : ", this.acceleration);
+    console.log("  inclination  : ", this.inclination);
+    console.log("  orientation  : ", this.orientation);
+    console.log("--------------------------------------");
+  });
+});
+	
+	
+Ricardo DeMatos's profile photo
+	Ricardo DeMatos
